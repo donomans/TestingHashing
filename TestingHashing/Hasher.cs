564dc40ceff7910431144603c9850423f8f87296
Donovan Sandey <fronn1@gmail.com>
2012-08-01 14:08:07 -0500

    Set up a few to test different hashing methods
    Added several different sized arrays and counted the collisions... so
    far everything is bad at 20 million elements. Hah.

---
+++ TestingHashing/Hasher.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingHashing
+{
+    public class Hasher
+    {
+        public IEnumerable<Hash> Hash(Object[] tohash, HashType type)
+        {
+            switch (type)
+            {
+                case HashType.RemoteMon: ///used in RemoteMon
+                    List<Hash> junkstring1hashes = new List<Hash>();
+                    foreach (var a in tohash)
+                        junkstring1hashes.Add(new Hash(UnsafeJunkString1Hash(a), a, type));
+                    return junkstring1hashes;
+
+                case HashType.FoundOnline1: ///found in hash table tester app
+                    return tohash.JunkHasher(a =>
+                    {
+                        String s = a.ToString();
+                        Int64 hash = 2166136261;
+		                foreach (char c in s)
+	                        hash = (16777619 * hash) ^ (Int32)c;
+
+                        return new Hash((Int32)hash, a, type);
+                    });
+                case HashType.GetHashCode:
+                    return tohash.JunkHasher(a =>
+                    {
+                        return new Hash(a.GetHashCode(), a, type);
+                    });
+                case HashType.MD5:
+                    return tohash.JunkHasher(a =>
+                    {
+                        return new Hash(BitConverter.ToInt32(
+                            HashAlgorithm.Create("MD5").ComputeHash(a.ToString().Select(s =>
+                                {
+                                    Byte[] b = new Byte[s.Length];
+                                    Int32 i = 0;
+                                    foreach (char c in s)
+                                        b[i++] = (Byte)c;
+                                    return b;
+                                })),
+                                0), a, type);
+                    });
+                case HashType.SHA1:
+                    return tohash.JunkHasher(a =>
+                    {
+                        return new Hash(BitConverter.ToInt32(
+                            HashAlgorithm.Create("SHA1").ComputeHash(a.ToString().Select(s =>
+                            {
+                                Byte[] b = new Byte[s.Length];
+                                Int32 i = 0;
+                                foreach (char c in s)
+                                    b[i++] = (Byte)c;
+                                return b;
+                            })),
+                                0), a, type);
+                    });
+                case HashType.SHA256:
+                    return tohash.JunkHasher(a =>
+                    {
+                        return new Hash(BitConverter.ToInt32(
+                             HashAlgorithm.Create("SHA256").ComputeHash(a.ToString().Select(s =>
+                             {
+                                 Byte[] b = new Byte[s.Length];
+                                 Int32 i = 0;
+                                 foreach (char c in s)
+                                     b[i++] = (Byte)c;
+                                 return b;
+                             })),
+                                 0), a, type); 
+                    });
+                case HashType.SHA512:
+                    return tohash.JunkHasher(a =>
+                    {
+                        return new Hash(BitConverter.ToInt32(
+                            HashAlgorithm.Create("SHA512").ComputeHash(a.ToString().Select(s =>
+                            {
+                                Byte[] b = new Byte[s.Length];
+                                Int32 i = 0;
+                                foreach (char c in s)
+                                    b[i++] = (Byte)c;
+                                return b;
+                            })),
+                                0), a, type);
+                    });
+               
+                default:
+                    return null;
+            }
+            
+        }
+
+        private unsafe Int32 UnsafeJunkString1Hash(Object a)
+        {
+            String s = a.ToString();
+            fixed (Char* str = s.ToCharArray())
+            {
+                Char* chPtr = str;
+                Int32 x = 0x34058547;
+                Int32 y = x;
+                Int32* intPtr = (Int32*)chPtr;
+                for (Int32 i = s.Length; i > 0; i -= 4)
+                {
+                    x = (((x << 5) + x) + (x >> 0x1B)) ^ intPtr[0];
+                    if (i <= 2)
+                    {
+                        break;
+                    }
+                    y = (((y << 5) + y) + (y >> 0x1B)) ^ intPtr[1];
+                    intPtr += 2;
+                }
+                return (x + (y * 0x214BD12C));
+            }
+        }
+
+      
+    }
+
+    public static class HasherExtension
+    {
+        public static IEnumerable<TResult> JunkHasher<T, TResult>(this IEnumerable<T> source, Func<T, TResult> hasher)
+        {
+            List<TResult> arr = new List<TResult>();
+            foreach (var item in source)
+                arr.Add(hasher(item));
+
+            return arr;
+        }
+
+        public static TResult Select<TResult>(this String source, Func<String, TResult> transform)
+        {
+            return transform(source);
+        }
+
+        public static void FillArray<T>(this T[] source, Func<T> fillarray)
+        {
+            for (Int32 i = 0; i < source.Length; i++)
+                source[i] = fillarray();
+        }
+
+        public static void FillArray(this String[] source)
+        {
+            Random r = new Random();
+            String UsableChars = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=:"";',./<>?";
+
+            for (Int32 count = 0; count < source.Length; count++)
+            {
+                Char[] chars = new Char[r.Next(5, 50)];
+                for (Int16 i = 0; i < chars.Length; i++)
+                {
+                    chars[i] = UsableChars[r.Next(0, UsableChars.Length)];
+                }
+                source[count] =  new String(chars);
+            }
+        }
+
+        public static Dictionary<T, R> Select<T, R, A>(this IEnumerable<A> source, Action<A, Dictionary<T,R>> transform)
+        {
+            Dictionary<T, R> dic = new Dictionary<T,R>();
+            foreach (var h in source)
+                transform(h, dic);
+
+            return dic;
+        }
+    }
+
+    public struct Hash
+    {
+        public Hash(Int32 hash, Object root, HashType hashtype = HashType.None)
+        {
+            HashCode = hash;
+            Original = root;
+            Type = hashtype;
+        }
+
+        public Object Original;
+        public HashType Type;
+        public Int32 HashCode;
+    }
+
+    public enum HashType
+    {
+        None,
+        RemoteMon,
+        FoundOnline1,
+        GetHashCode,
+        MD5,
+        SHA1,
+        SHA256,
+        SHA512,
+        JunkObject8,
+        JunkObject9
+    }
+}
--- TestingHashing/Program.cs
+++ TestingHashing/Program.cs
@@ -8,8 +8,91 @@ namespace TestingHashing
 {
     class Program
     {
+        const String UsableChars = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=:"";',./<>?";
+
+
         static void Main(string[] args)
         {
+            ///Create 3 arrays of varying size
+            
+            RunHash(HashType.FoundOnline1);
+            RunHash(HashType.GetHashCode);
+            RunHash(HashType.MD5);
+            RunHash(HashType.RemoteMon);
+            RunHash(HashType.SHA1);
+            RunHash(HashType.SHA256);
+            RunHash(HashType.SHA512);
+            
+            Console.ReadLine();
         }
+
+        private static void RunHash(HashType hashtype)
+        {
+            Random r = new Random();
+            DateTime starttime = DateTime.Now;
+
+            String[] SmallArray = new String[5500];
+            String[] MediumArray = new String[60000];
+            String[] LargeArray = new String[500000];
+            String[] SuperArray = new String[20000000];
+            ///fill them with junk data
+            SmallArray.FillArray();
+            MediumArray.FillArray();
+            LargeArray.FillArray();
+            SuperArray.FillArray();
+
+            Hasher hasher = new Hasher();
+
+            
+            Dictionary<Int32, Int32> smallarraydistribution =
+                hasher.Hash(SmallArray, hashtype).Select<Int32, Int32, Hash>((h, d) =>
+                {
+                    if (d.ContainsKey(h.HashCode))
+                        d[h.HashCode] += 1;
+                    else
+                        d.Add(h.HashCode, 1);
+                });
+
+            Dictionary<Int32, Int32> mediumarraydistribution =
+                hasher.Hash(MediumArray, hashtype).Select<Int32, Int32, Hash>((h, d) =>
+                {
+                    if (d.ContainsKey(h.HashCode))
+                        d[h.HashCode] += 1;
+                    else
+                        d.Add(h.HashCode, 1);
+                });
+
+            Dictionary<Int32, Int32> largearraydistribution =
+                hasher.Hash(LargeArray, hashtype).Select<Int32, Int32, Hash>((h, d) =>
+                {
+                    if (d.ContainsKey(h.HashCode))
+                        d[h.HashCode] += 1;
+                    else
+                        d.Add(h.HashCode, 1);
+                });
+
+            Dictionary<Int32, Int32> superarraydistribution =
+                hasher.Hash(SuperArray, hashtype).Select<Int32, Int32, Hash>((h, d) =>
+                {
+                    if (d.ContainsKey(h.HashCode))
+                        d[h.HashCode] += 1;
+                    else
+                        d.Add(h.HashCode, 1);
+                });
+
+            Int32 smallcollisions = smallarraydistribution.Count(d => d.Value > 1);
+            Int32 mediumcollisions = mediumarraydistribution.Count(d => d.Value > 1);
+            Int32 largecollisions = largearraydistribution.Count(d => d.Value > 1);
+            Int32 supercollisions = superarraydistribution.Count(d => d.Value > 1);
+            Console.WriteLine("Hash type: " + hashtype.ToString());
+            Console.WriteLine("Small Array[" + SmallArray.Length + "] Collisions: " + smallcollisions);
+            Console.WriteLine("Medium Array[" + MediumArray.Length + "] Collisions: " + mediumcollisions);
+            Console.WriteLine("Large Array[" + LargeArray.Length + "] Collisions: " + largecollisions);
+            Console.WriteLine("Super Array[" + SuperArray.Length + "] Collisions: " + supercollisions);
+            Console.WriteLine("Timer: " + (DateTime.Now - starttime).TotalSeconds);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
     }
 }
